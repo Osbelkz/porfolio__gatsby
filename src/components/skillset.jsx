fca35e1de61b257904ef9e631599c5c898a31ab4
Askar <osbelkz@gmail.com>
2020-12-25 14:02:12 +0600

    add skillset

--- src/components/skillset.jsx
+++ src/components/skillset.jsx
@@ -17,32 +17,29 @@ const Skillset = () => {
         </div>
         <div className={styles.skills}>
           <div className={styles.skill}>
-            <img src={productImg} alt="" />
+            <img src={productImg} alt="product image" />
             <h3 className={styles.title}>Product Design</h3>
             <p className={styles.text}>
               Working at Facebook has taught me a lot about how to understand users, solve problems and build great products.
             </p>
           </div>
           <div className={styles.skill}>
-            <img src="" alt="" />
-            <h3 className={styles.title}>Product Design</h3>
+            <img src={visualImg} alt="visual image" />
+            <h3 className={styles.title}>Visual Design</h3>
             <p className={styles.text}>
-              Working at Facebook has taught me a lot about how to understand users, solve problems and build great products.
-            </p>
+              My experience at dribbble has helped me learn to develop the eye for design. Colors, typography, layout and the whole package.            </p>
           </div>
           <div className={styles.skill}>
-            <img src="" alt="" />
-            <h3 className={styles.title}>Product Design</h3>
+            <img src={motionImg} alt="motion image" />
+            <h3 className={styles.title}>Motion Design</h3>
             <p className={styles.text}>
-              Working at Facebook has taught me a lot about how to understand users, solve problems and build great products.
-            </p>
+              I started my design journey with motion design in my college days. Motion is something that really fascinates me because of the flexibility of story telling.             </p>
           </div>
           <div className={styles.skill}>
-            <img src="" alt="" />
-            <h3 className={styles.title}>Product Design</h3>
+            <img src={photographyImg} alt="photography image" />
+            <h3 className={styles.title}>Photography</h3>
             <p className={styles.text}>
-              Working at Facebook has taught me a lot about how to understand users, solve problems and build great products.
-            </p>
+              Clicking pictures really brings out the creative in me. Phtography really makes you look and percieve things in a different way.            </p>
           </div>
         </div>
       </div>
